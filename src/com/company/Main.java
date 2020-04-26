8923bd8795052b0ba5a39aaa6d30743578a61be1
ajdamiao <ajdamiaof@gmail.com>
2020-04-26 15:29:31 -0300

    Adição para casos do mundo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,9 +23,12 @@ public class Main {
         System.out.println("===============================");
         while (true) {
             final String url = "https://www.worldometers.info/coronavirus/country/brazil/";
+            final String url2 = "https://www.worldometers.info/coronavirus/";
             String numeroCasos = null, mortes = null, curados = null;
+            String numeroCasosMundo = null, mortesMundo = null, curadosMundo = null;
 
             try {
+                //BRASIL
                 final Document document = Jsoup.connect(url).get();
                 String informacoes = null;
                 int cont = 0;
@@ -43,8 +46,32 @@ public class Main {
                         curados = informacoes;
                     }
                 }
-                String mensagem = ("Total de casos no Brasil:\n" + "Casos confirmados: " + numeroCasos + "\n" + "Óbitos: " + mortes + "\n" + "Recuperações: " + curados + "\n"+ "#coronavirus #corona" + "\n\n");
+                String mensagem = ("Total de casos no Brasil:\n" + "Casos confirmados: " + numeroCasos + "\n" + "Óbitos: " + mortes + "\n" + "Recuperações: " + curados + "\n");
+                //FIM BRASIL
+
+                // MUNDO
+                final Document document2 = Jsoup.connect(url2).get();
+                String informacoes2 = null;
+                int cont2 = 0;
+                for (Element row : document2.select("div.maincounter-number")) {
+                    cont2 = cont2 + 1;
+                    informacoes2 = row.select(".maincounter-number").text();
+                    // GAMBIARRA (a cada volta no FOR o cont2 acrescenta em 1 para poder armazenar as informacoes em variaveis foras do FOR)
+                    if (cont2 == 1) {
+                        numeroCasosMundo = informacoes2;
+                    }
+                    if (cont2 == 2) {
+                        mortesMundo = informacoes2;
+                    }
+                    if (cont2 == 3) {
+                        curadosMundo = informacoes2;
+                    }
+                }
+                String mensagem2 = ("Total de casos no mundo:\n" + "Casos confirmados: " + numeroCasosMundo + "\n" + "Óbitos: " + mortesMundo + "\n" + "Recuperações: " + curadosMundo + "\n"+ "#coronavirus #corona" + "\n\n");
+                // FIM MUNDO
+
                 System.out.println(mensagem);
+                System.out.println(mensagem2);
 
                 // HTTP REQUEST
                 URL urlZapier = new URL("https://hooks.zapier.com/hooks/catch/7355727/o5xs2za");
@@ -55,6 +82,7 @@ public class Main {
 
                 Map<String, String> arguments = new HashMap<>();
                 arguments.put("value1", mensagem);
+                arguments.put("value2", mensagem2);
                 StringJoiner sj = new StringJoiner("&");
                 for (Map.Entry<String, String> entry : arguments.entrySet())
                     sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
